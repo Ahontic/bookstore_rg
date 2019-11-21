02de97eebd68b034d9248ca49ab97e367c923e9b
DELL <yevhenii@rubygarage.org>
2019-11-21 19:02:21 +0200

    Pages controller/ Layout split

--- spec/controllers/home_controller_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.describe HomeController, type: :controller do
-  describe 'GET #home' do
-    it 'returns http success' do
-      expect(response).to be_ok
-    end
-  end
-end
