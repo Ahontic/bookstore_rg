f7340cdbf10be1cfdf14ff6ee00bf364ec2822e8
DELL <yevhenii@rubygarage.org>
2019-11-28 18:59:29 +0200

    Fixed tests

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-  resources :books, only: [:show]
+  resources :books, only: [:index, :show]
   resources :category, only: [:index, :show] do
     resources :books, only: [:index, :show]
   end
--- spec/controllers/catalog_controller_spec.rb
+++ spec/controllers/catalog_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe CatalogController, type: :controller do
   describe 'GET #index' do
     it 'returns http success' do
       get :index
-      expect(response).to have_http_status(:success)
+      expect(response).to be_ok
     end
   end
 end
