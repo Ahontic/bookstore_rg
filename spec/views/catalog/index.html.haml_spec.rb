89dc85483fa003a60df0087e7e82d4a829364c4b
DELL <yevhenii@rubygarage.org>
2019-12-02 13:42:26 +0200

    config.force_ssl = true

--- spec/views/catalog/index.html.haml_spec.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.describe 'catalog/index.html.haml', type: :view do
-end
--- spec/views/home/index.html.erb_spec.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.describe 'home/index.html.erb', type: :view do
-end
