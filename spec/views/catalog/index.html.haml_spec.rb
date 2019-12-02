0ebee81dd8605631205cff24c32085645fa85639
DELL <yevhenii@rubygarage.org>
2019-12-02 11:59:14 +0200

    Spec views

---
+++ spec/views/catalog/index.html.haml_spec.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+RSpec.describe 'catalog/index.html.haml', type: :view do
+end
---
+++ spec/views/home/index.html.erb_spec.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+RSpec.describe 'home/index.html.erb', type: :view do
+end
