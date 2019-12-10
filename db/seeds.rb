cdb41cad304247f97c70eb901f4ac763121ca309
DELL <yevhenii@rubygarage.org>
2019-12-10 13:43:13 +0200

    Fixed categories seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-# data = ['Mobile Development', 'Photo', 'Web Design']
-# categories = data.map { |category| FactoryBot.build(:category, category) }
-
 # Seeding Authors
 FactoryBot.create_list(:author, 5)
 # Seeding Books
 FactoryBot.create_list(:book, 20)
 # Seeding Materials
 FactoryBot.create_list(:material, 3)
+# Seeding Category
+categories = ['Mobile Development', 'Photo', 'Web Design']
+categories.each { |category| Category.create(title: category) }
