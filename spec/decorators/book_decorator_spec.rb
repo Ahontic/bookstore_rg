6c50921d63a7846cda2e50740e0d0ad11fc33d29
DELL <yevhenii@rubygarage.org>
2019-12-12 11:00:49 +0200

    BookDecorator_spec

--- spec/decorators/book_decorator_spec.rb
+++ spec/decorators/book_decorator_spec.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
-
 RSpec.describe BookDecorator do
+  let(:book) { create(:book, :with_author).decorate }
+
+  describe 'decorates' do
+    example 'dimensions' do
+      expect(book.dimensions).to eq("H:#{book.height}\" x W: #{book.width}\" x D: #{book.depth}\"")
+    end
+
+    example 'authors names ' do
+      expect(book.authors_names).to eq(book.authors.reduce.full_name)
+    end
+  end
 end
