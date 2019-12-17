5e8ed07ace7bd1d8e9ed1fde88065726dfc8bbcf
DELL <yevhenii@rubygarage.org>
2019-12-17 19:12:38 +0200

    Added devise secure_validate

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "bookstore_rg_production"
-  config.action_mailer.default_url_options = { host: 'https://staging-damp-dawn-43764.herokuapp.com/' }
+  config.action_mailer.default_url_options = ENV['host']
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
