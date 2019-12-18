45fb4798c8e34e3a5f544103487dc0bfb16c51a6
DELL <yevhenii@rubygarage.org>
2019-12-18 16:05:19 +0200

    Customer feature tests added

--- spec/support/session_helper.rb
+++ spec/support/session_helper.rb
@@ -4,8 +4,27 @@ def sign_up
   visit new_customer_registration_path
 
   fill_in :email, with: FFaker::Internet.disposable_email
-  fill_in :password, with: FFaker::Internet.password
-  fill_in :password_confirmation, with: FFaker::Internet.password
+  fill_in :password, with: '7qLyxOACqQ'
+  fill_in :password_confirmation, with: '7qLyxOACqQ'
 
   click_on 'Sign up'
+end
+
+def sign_up_with(email: FFaker::Internet.disposable_email, password: '7qLyxOACqQ')
+  visit new_customer_registration_path
+
+  fill_in :email, with: email
+  fill_in :password, with: password
+  fill_in :password_confirmation, with: password
+
+  click_on 'Sign Up'
+end
+
+def sign_in_with(email: FFaker::Internet.disposable_email, password: '')
+  visit new_customer_session_path
+
+  fill_in :email, with: email
+  fill_in :password, with: password
+
+  click_on 'Back to Store'
 end
