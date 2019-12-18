92f4b0de3c9f8716cbd732d9ed8729b6690397d3
DELL <yevhenii@rubygarage.org>
2019-12-18 18:53:08 +0200

    Config production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,18 +66,18 @@ Rails.application.configure do
   config.action_mailer.default_url_options = ENV['host']
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default :charset => “utf-8”
+  config.action_mailer.default charset: 'utf-8'
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_options = { from: “hwpauler@gmail.com” }
+  config.action_mailer.default_options = { from: 'hwpauler@gmail.com' }
   config.action_mailer.smtp_settings = {
-     address: “smtp.gmail.com”,
-     port: 587,
-     authentication: “plain”,
-     enable_starttls_auto: true,
-     user_name: ENV[‘GMAIL_USERNAME’],
-     password: ENV[‘GMAIL_PASSWORD’],
-     openssl_verify_mode: ‘none’
-     }
+    address: 'smtp.gmail.com',
+    port: 587,
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD'],
+    openssl_verify_mode: 'none'
+  }
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
