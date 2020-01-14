46b52469076f159707008127b8ce50f7c72a6c21
DELL <yevhenii@rubygarage.org>
2020-01-14 15:32:28 +0200

    Fixed reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 class ReviewsController < ApplicationController
+  decorates_assigned :book
+
   def create
     @review = Review.new(review_params)
+    @book = BookDecorator.find_by(id: params[:book_id])
     if @review.save
       flash[:notice] = 'Thanks for Review. It will be published as soon as Admin will approve it.'
       redirect_back(fallback_location: root_path)
