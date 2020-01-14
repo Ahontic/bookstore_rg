f18444cdde5aa9945a368b992408ab0de7fb4dba
DELL <yevhenii@rubygarage.org>
2020-01-14 13:09:21 +0200

    Added Active admin resources

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register Customer do
+  # Customize columns displayed on the index screen in the table
+  index do
+    selectable_column
+    id_column
+    column :email
+    column :current_sign_in_at
+    column :created_at
+    actions
+  end
 end
