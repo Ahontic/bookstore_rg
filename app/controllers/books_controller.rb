64e46de19aab8d1a5d9b9d73cfb6f29e4e10d3e8
DELL <yevhenii@rubygarage.org>
2020-01-14 18:01:10 +0200

    Render review partial instead of instance var

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,13 +4,7 @@ class BooksController < ApplicationController
   decorates_assigned :book
 
   def show
-    @book = BookDecorator.find_by(id: params[:id], category_id: params[:category_id]) if legal_partial?
+    @book = BookDecorator.find_by(id: params[:id], category_id: params[:category_id])
     @reviews = @book.reviews.approved
-  end
-
-  private
-
-  def legal_partial?
-    Book.ids.to_s.include? params[:id]
   end
 end
