bbec6a54dd58f68c981b7de183832479c07bf14e
DELL <yevhenii@rubygarage.org>
2020-01-17 15:48:32 +0200

    Added cloudinary storage config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,9 +30,6 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Use a real queuing backend for Active Job (and separate queues per environment).
-  config.active_job.queue_adapter = :sidekiq
-
   # Store uploaded files on the local file system (see config/storage.yml for options).
 
   config.active_storage.service = :local
@@ -44,18 +41,6 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    domain: 'google.com',
-    authentication: 'plain',
-    enable_starttls_auto: true,
-    user_name: ENV['GMAIL_USERNAME'],
-    password: ENV['GMAIL_PASSWORD']
-  }
-
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.smtp_settings = {
--- db/migrate/.20191226160213_add_address_type_to_addresses.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddAddressTypeToAddresses < ActiveRecord::Migration[6.0]
-  def change
-    add_column :addresses, :address_type, :string, null: false, default: 0
-  end
-end
