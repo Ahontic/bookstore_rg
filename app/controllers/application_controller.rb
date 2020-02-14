73ac4a4336bce8b37e86731a044adf54761512fc
DELL <yevhenii@rubygarage.org>
2020-02-14 12:03:49 +0200

    Added protected to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,8 @@ class ApplicationController < ActionController::Base
     render file: Rails.public_path.join('404.html'), status: :not_found, layout: false
   end
 
+  protected
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name avatar])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[name avatar])
