e97dd5ceb553b45bef0a5700d7896181a6454fd1
DELL <yevhenii@rubygarage.org>
2020-02-21 15:40:26 +0200

    Fixed checkout address checkbox value to true|nil

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,5 +1,32 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: carts
+#
+#  id             :bigint           not null, primary key
+#  number         :string
+#  status         :integer
+#  use_billing    :boolean          default(FALSE)
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  credit_card_id :bigint
+#  customer_id    :bigint
+#  delivery_id    :bigint
+#
+# Indexes
+#
+#  index_carts_on_credit_card_id  (credit_card_id)
+#  index_carts_on_customer_id     (customer_id)
+#  index_carts_on_delivery_id     (delivery_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (credit_card_id => credit_cards.id)
+#  fk_rails_...  (customer_id => customers.id)
+#  fk_rails_...  (delivery_id => deliveries.id)
+#
+
 RSpec.describe Cart do
   describe 'associations' do
     it { is_expected.to have_many(:line_items) }
